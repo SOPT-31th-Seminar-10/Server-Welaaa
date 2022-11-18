99bdf7d5ce045cbc4890a5511b97213e783b325f
sunny <huiseon37@gmail.com>
2022-11-18 22:32:25 +0900

    [feat] getCategory api 구현

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import router from "./router";
 
 const app = express();
@@ -6,12 +6,7 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.use("/api", router); 
-
-//* HTTP method - GET
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("윌라 서버 오픈~!");
-});
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`
